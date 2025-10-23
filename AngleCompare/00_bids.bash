ee901c9ea77b822ab7b2f2f55c154075393e619e
WillForan <willforan@gmail.com>
2025-10-22 20:46:52 -0400

    AngleCompare: bids 10x rest at each angle

--- AngleCompare/00_bids.bash
+++ AngleCompare/00_bids.bash
@@ -2,11 +2,78 @@
 #
 # pilot to bids for fmriprep, sdcflows, etc
 #
+#
+set -euo pipefail
 # 20251017WF - init
+#     1020 - phantom
+#     1022 - WF & CM data
 export PATH="/opt/ni_tools/lncdtools:/opt/ni_tools/afni:/opt/ni_tools/fsl:$PATH"
 
+##
+#
+#ls /mnt/TWIX_RAID/Prisma2/Moon/BOLDSliceOrient/2025-10-22/Subject1/DICOM/ |   perl -F_ -slane 'push @a, {p=>$_,s=>$F[$#f]}; END{print join("\n", map {$_->{p}} (sort {$a->{s} <=> $b->{s}} @a));}'
+#
+#
+
+bids="$(cd $(dirname "$0")/../Data/; pwd)/bids-a10"
+mkdir -p $bids
+for dcmdir in /mnt/TWIX_RAID/Prisma2/Moon/BOLDSliceOrient/2025-10-22/Subject[12]/DICOM/; do
+  ! [[ "$dcmdir" =~ Subject([0-9]) ]] && echo "ERROR: bad Subject match in '$dcmdir'" && continue
+  subj=${BASH_REMATCH[1]}
+
+  mknii $bids/sub-$subj/anat/sub-${subj}_T1w.nii.gz $dcmdir/T1MPRAGE_000*
+
+  mknii $bids/sub-$subj/fmap/sub-${subj}_acq-largefov_magnitude.nii.gz $dcmdir/GRE_FIELDMAP_LARGEFOV_0003
+  mknii $bids/sub-$subj/fmap/sub-${subj}_acq-largefov_phasediff.nii.gz $dcmdir/GRE_FIELDMAP_LARGEFOV_0004
+  [[ $subj == 2 ]] && inc_num=7 || inc_num='*'
+  mknii $bids/sub-$subj/func/sub-${subj}_task-n40p20_acq-inc_bold.nii.gz $dcmdir/A_EP2D_BOLD_ANG_N40P20_2MM_ASCEND_INCANG_000$inc_num
+  : mknii $bids/sub-$subj/func/sub-${subj}_task-n40p20_acq-dec_bold.nii.gz $dcmdir/A_EP2D_BOLD_ANG_N40P20_2MM_ASCEND_DECANG_000*
+  : mknii $bids/sub-$subj/func/sub-${subj}_task-n40p20_acq-mid_bold.nii.gz $dcmdir/A_EP2D_BOLD_ANG_N40P20_2MM_ASCEND_MIDANG_000*
+
+  mknii $bids/sub-$subj/func/sub-${subj}_task-n40p20_run-2_acq-inc_bold.nii.gz $dcmdir/A_EP2D_BOLD_ANG_N40P20_2MM_ASCEND_INCANG_006* || :
+  mknii $bids/sub-$subj/func/sub-${subj}_task-n40p20_run-2_acq-dec_bold.nii.gz $dcmdir/A_EP2D_BOLD_ANG_N40P20_2MM_ASCEND_DECANG_006* || :
+  mknii $bids/sub-$subj/func/sub-${subj}_task-n40p20_run-2_acq-mid_bold.nii.gz $dcmdir/A_EP2D_BOLD_ANG_N40P20_2MM_ASCEND_MIDANG_006* || :
+
+  add-intended-for -fmap '*acq-largeFOV*phasediff*.json' -for '*task-n40p20*.nii.gz' $bids/$subj
+  add-intended-for -fmap '*acq-largeFOV*magnitude*.json' -for '*task-n40p20*.nii.gz' $bids/$subj
+
+  #mknii $bids/sub-$subj/func/sub-${sub}_task-restn39_sbref.nii.gz $dcmdir/BOLD_REST_AP_T_C-39_9_SBREF_0009
+  #mknii $bids/sub-$subj/func/sub-${sub}_task-restn39_bold.nii.gz $dcmdir/BOLD_REST_AP_T_C-39_9_0010
+  #mknii $bids/sub-$subj/fmap/sub-${sub}_acq-restn39_dir-AP_epi.nii.gz $dcmdir/SPINECHOFIELDMAP_AP_T_C-39_9_0012
+  #mknii $bids/sub-$subj/fmap/sub-${sub}_acq-restn39_dir-PA_epi.nii.gz $dcmdir/SPINECHOFIELDMAP_PA_T_C-39_9_0013
+  for d in $(ls -d $dcmdir/*{BOLD_REST,SPINECHOFIELDMAP_}* | perl -F_ -slane 'push @a, {p=>$_,s=>$F[$#f]}; END{print join("\n", map {$_->{p}} (sort {$a->{s} <=> $b->{s}} @a));}' ); do
+  	! [[ $d =~ T_C([-0-9]+) ]] && echo "ERROR: bad match for T_C in '$d'" && continue
+  	taskname=${BASH_REMATCH[1]}
+  	taskname=rest${taskname//-/n}
+  	case $d in 
+		*PHYSIO*)  : echo "# skipping phyiso '$d'"; continue ;;
+  		*REST*SBREF*) out=func/sub-${subj}_task-${taskname}_sbref.nii.gz;;
+  		*REST*) out=func/sub-${subj}_task-${taskname}_bold.nii.gz;;
+  		*SPINECHOFIELDMAP_AP*) out=fmap/sub-${subj}_acq-${taskname}_dir-AP_epi.nii.gz;;
+  		*SPINECHOFIELDMAP_PA*) out=fmap/sub-${subj}_acq-${taskname}_dir-PA_epi.nii.gz;;
+  		*) echo "ERROR: dir does not match rest or spin echo name! '$d'"; continue ;;
+  	esac
+  	mknii "$bids/sub-$subj/$out" "$d" || echo "mknii error '$?'; continuing anyway"
+  done
+
+  for taskname in $(ls $bids/sub-$subj/func/*task-*.nii.gz | grep -Po '(?<=task-)[^_-]*'|sort -u|grep -v n40p20); do
+     add-intended-for -fmap "*acq-$taskname*epi.json" -for "*task-$taskname*.nii.gz" $bids/sub-$subj/
+  done
+done
+
+for f in $bids/*/func/*task*_bold.json; do
+  [[ $f =~ task-([^_-]*) ]] || continue
+  grep TaskName $f ||
+    sed -i "1s;^{;{\n\"TaskName\": \"${BASH_REMATCH[1]}\",;" $f
+done
+
+
+exit
+
+
+
 ## 2025
-dcmdir="../Data/2025-10-20/"
+dcmdir="../Data/2025-10-20/DICOM/"
 bids="$(cd $(dirname "$0")/../Data/; pwd)/bids-phantom"
 sub=sub-20251020
 
--- AngleCompare/01_preproc_fmriprep.bash
+++ AngleCompare/01_preproc_fmriprep.bash
@@ -5,26 +5,34 @@ VER=25.2.3
 #podman image ls
 # docker.io/nipreps/fmriprep   25.2.3                   dd3791489f2a  2 hours ago    7.14 GB
 datadir="$(cd "$(dirname "$0")/../Data"; pwd)"
+#  BIDS=${BIDS:-$datadir/bids}
+  BIDS=${BIDS:-$datadir/bids-a10} # 20251022
+bidsdb=$datadir/db/$(basename $BIDS)
+
    OUT=$datadir/preproc/fmriprep-$VER
  FSOUT=$datadir/preproc/fs-fmriprep-$VER
   WORK=$datadir/preproc/fmriprep-$VER-work
-  BIDS=$datadir/bids
-bidsdb=$datadir/bids-db
 
 mkdir -p $OUT $FSOUT $WORK $bidsdb
 
 export FS_LICENSE=$PWD/fs_license.txt TEMPLATEFLOW_HOME=$HOME/.templateflow
 
 # doesn't matter here. but for large bids dataset, wouldn't want to regenerate
-: uv run --with pybids \
+test -r $bidsdb/layout_index.sqlite ||
+  dryrun uv run --with pybids \
     pybids layout $BIDS $bidsdb --no-validate --index-metadata
 
 # grab templates and make avial to container. dont want to pull them every new run
-: uv run --with templateflow \
+test -s $TEMPLATEFLOW_HOME/tpl-MNI152NLin2009bAsym/tpl-MNI152NLin2009bAsym_res-1_T1w.nii.gz ||
+  dryrun uv run --with templateflow \
    python -c "from templateflow.api import get; get(['MNI152NLin2009cAsym', 'MNI152NLin6Asym'])"
 
 # quick check bids is valid
-: podman run -v $BIDS:$BIDS:ro --entrypoint bids-validator docker.io/nipreps/fmriprep:25.2.3 $BIDS
+echo podman run -v $BIDS:$BIDS:ro --entrypoint bids-validator docker.io/nipreps/fmriprep:25.2.3 $BIDS
+
+# don't use for phanom. no anat to align, no point. see sdcflow instead
+#phantom_opts=()
+#[[ $BIDS =~ phatnom ]] && phantom_opts=(--fs-no-reconall --output-spaces )
 
 dryrun podman \
   run \
@@ -39,8 +47,9 @@ dryrun podman \
   --env FS_LICENSE \
   --env TEMPLATEFLOW_HOME \
   docker.io/nipreps/fmriprep:$VER \
+  "$@" \
   --bids-database-dir $bidsdb \
   --fs-subjects-dir $FSOUT \
   -w $WORK \
-  "$@" \
+  "${phantom_opts[@]}" \
   $BIDS $OUT participant
