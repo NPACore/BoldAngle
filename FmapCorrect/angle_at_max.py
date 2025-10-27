98adaddd10bb68d5bf1d64e2950e4918dd535977
WillForan <willforan@gmail.com>
2025-10-26 20:53:56 -0400

    angle_at_max.py: lint (black, isort)

--- FmapCorrect/angle_at_max.py
+++ FmapCorrect/angle_at_max.py
@@ -1,28 +1,55 @@
 #!/usr/bin/env python3
 import argparse
-import numpy as np
-import nibabel as nib
 import os
 import sys
 
-parser = argparse.ArgumentParser(description="Report max normalized intensity per voxel")
-parser.add_argument('-i', '--input', type=str, required=True, help="Path to epi file.")
-parser.add_argument('-m', '--mask', type=str, required=True, help="Path to mask file.")
-parser.add_argument('-o', '--output', type=str, required=True, help="Path to save file.")
-parser.add_argument('-n', '--normed_out', type=str, required=False, help="Where to save SD normed file.", default=None)
-parser.add_argument('-l', '--labelfile', type=str, required=False, help="File containing label values (float)", default="angle.txt")
-parser.add_argument('--nosd', required=False, help="Do not norm by standard devatoin", action='store_true', default=False)
+import nibabel as nib
+import numpy as np
+
+parser = argparse.ArgumentParser(
+    description="Report max normalized intensity per voxel"
+)
+parser.add_argument("-i", "--input", type=str, required=True, help="Path to epi file.")
+parser.add_argument("-m", "--mask", type=str, required=True, help="Path to mask file.")
+parser.add_argument(
+    "-o", "--output", type=str, required=True, help="Path to save file."
+)
+parser.add_argument(
+    "-n",
+    "--normed_out",
+    type=str,
+    required=False,
+    help="Where to save SD normed file.",
+    default=None,
+)
+parser.add_argument(
+    "-l",
+    "--labelfile",
+    type=str,
+    required=False,
+    help="File containing label values (float)",
+    default="angle.txt",
+)
+parser.add_argument(
+    "--nosd",
+    required=False,
+    help="Do not norm by standard devatoin",
+    action="store_true",
+    default=False,
+)
 args = parser.parse_args()
 
 ep = nib.load(args.input)
 mask = nib.load(args.mask)
 
-ep_brain = np.where(np.asanyarray(mask.dataobj)>0,1, np.nan)
+ep_brain = np.where(np.asanyarray(mask.dataobj) > 0, 1, np.nan)
 if args.nosd:
     sd = np.ones(ep.dataobj.shape)
 else:
-    sd = np.std(ep.dataobj, axis=(0,1,2), where=np.isfinite(ep_brain[...,np.newaxis]))
-mx = np.argmax(ep.dataobj/sd, axis=3)
+    sd = np.std(
+        ep.dataobj, axis=(0, 1, 2), where=np.isfinite(ep_brain[..., np.newaxis])
+    )
+mx = np.argmax(ep.dataobj / sd, axis=3)
 
 an = np.loadtxt(args.labelfile, dtype=float)
 angle_3d = an[mx] * ep_brain
@@ -35,5 +62,5 @@ notes = f'AFNI_NIFTI_TYPE_WARN=NO 3dNotes -h "{" ".join(sys.argv)}" "{args.outpu
 os.system(notes)
 
 if args.normed_out:
-    vis = ep.dataobj * sd;
-    nib.save(nib.Nifti1Image(vis, ep.affine,ep.header), args.normed_out)
+    vis = ep.dataobj * sd
+    nib.save(nib.Nifti1Image(vis, ep.affine, ep.header), args.normed_out)
