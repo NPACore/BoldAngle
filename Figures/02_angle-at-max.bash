93578aa924c22871e0cd4b78fdd36aa23d785b69
WillForan <willforan@gmail.com>
2025-10-27 08:47:08 -0400

    angle_at_max for tsnr

--- Figures/02_angle-at-max.bash
+++ Figures/02_angle-at-max.bash
@@ -17,3 +17,23 @@ slicer_big(){ slice -a >(gm convert -scale 200% - "$1") "$2"; }
 slicer_big angle-at-max/sub-1_aam-nosd.png  angle-at-max/sub1_nosd.nii.gz
 slicer_big angle-at-max/sub-1_aam.png  $fmapsub1/angles_at_max.nii.gz
 slicer_big angle-at-max/sub-1_phasediff.png ../FmapCorrect/wf/human-largefov/sub-1/sub-1_acq-largefov_phasediff.nii.gz
+
+3dTcat -prefix angle-at-max/sub-1_tsnr-all.nii.gz \
+	../Data/tsnr/sub-1_task-restn39_tsnr.nii.gz \
+	../Data/tsnr/sub-1_task-restn33_tsnr.nii.gz \
+	../Data/tsnr/sub-1_task-restn26_tsnr.nii.gz \
+	../Data/tsnr/sub-1_task-restn19_tsnr.nii.gz \
+	../Data/tsnr/sub-1_task-restn13_tsnr.nii.gz \
+	../Data/tsnr/sub-1_task-restn6_tsnr.nii.gz \
+	../Data/tsnr/sub-1_task-rest0_tsnr.nii.gz \
+	../Data/tsnr/sub-1_task-rest6_tsnr.nii.gz\
+	../Data/tsnr/sub-1_task-rest13_tsnr.nii.gz \
+	../Data/tsnr/sub-1_task-rest20_tsnr.nii.gz
+
+../FmapCorrect/angle_at_max.py  \
+	--nosd \
+	-l ../FmapCorrect/angle.txt \
+	-i angle-at-max/sub-1_tsnr-all.nii.gz  \
+	-m ../Data/preproc/fmriprep-25.2.3/sub-1/func/sub-1_task-rest0_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz  \
+	-o angle-at-max/sub-1_angleatmax-tsnr.nii.gz 
+	
