ed0c7ae539a80200d2cb237ea142fa7095f6a647
WillForan <willforan@gmail.com>
2025-10-27 08:47:27 -0400

    wip: plot anat and b0 w/ tsnr

--- AngleCompare/03a_tsnr_cmp.R
+++ AngleCompare/03a_tsnr_cmp.R
@@ -6,7 +6,7 @@
 
 # 20251023WF - init
 pacman::p_load(dplyr, tidyr, oro.nifti, ggplot2, stringr)
-pacman::p_install(ggExtra) # dont shadow any other functions
+#pacman::p_install(ggExtra) # dont shadow any other functions
 tsnr_fname <- Sys.glob('../Data/tsnr/sub-[12]_task-rest*_tsnr.nii.gz')
 angle_from_taskname <- function(n) str_extract(n, '(?<=task-rest)[^/_-]+') |> gsub(pat='n',rep='-') |> as.numeric()
 mni_mask <- oro.nifti::readNIfTI('mni_brainmask.nii.gz')@.Data
@@ -78,30 +78,32 @@ anglemax3d[alltsnr_sub1[,,,1]==0] <- NA
 angles <- angle_from_taskname(tsnr_fname[subj1_idx])
 anglemax3d <- angles[anglemax3d] 
 dim(anglemax3d) <- dim(alltsnr_sub1)[1:3]
-rslice_ys <- lapply(c(20,45,65),
-                 \(y) reshape2::melt(tmp[,y,],
-                                     varnames=c("x","z"),
-                                     value.name='tsnr') |>
-                      mutate(y=y)) |>
-    bind_rows()
+rslice_ys <- function(data)
+    lapply(c(20,45,65),
+           \(y) reshape2::melt(data[,y,],
+                               varnames=c("x","z"),
+                               value.name='tsnr') |>
+                mutate(y=y)) |>
+        bind_rows()
 
-rslice_zs <- lapply(c(25,55, 65),
-                 \(z) reshape2::melt(tmp[,,z],
-                                     varnames=c("x","y"),
-                                     value.name='tsnr') |>
-                      mutate(z=z)) |>
-    bind_rows()
+rslice_zs <- function(data)
+    lapply(c(25,55, 65),
+           \(z) reshape2::melt(data[,,z],
+                               varnames=c("x","y"),
+                               value.name='tsnr') |>
+                mutate(z=z)) |>
+        bind_rows()
 
 cowplot::plot_grid(ncol=3,
  tsnr_plane,
- rslice_ys |>
+ rslice_ys(anglemax3d) |>
   ggplot(aes(x = x, y = z, fill = tsnr)) +
   scale_fill_gradient2(high='red',mid='green',low='blue',na.value='#FFFFFF00') +
   facet_grid(y~.) +
   geom_raster() +
   theme_minimal() +
   guides(fill="none"),
- rslice_zs |>
+ rslice_zs(anglemax3d) |>
   ggplot(aes(x = x, y = y, fill = tsnr)) +
   scale_fill_gradient2(high='red',mid='green',low='blue',na.value='#FFFFFF00') +
   facet_grid(z~.) +
@@ -109,12 +111,50 @@ cowplot::plot_grid(ncol=3,
     theme_minimal() +
   labs(fill='Angle of\nmax tSNR') )
 
-
-
-p<- rslice_zs |>
+p<- rslice_zs(anglemax3d) |>
   ggplot(aes(x = x, y = y, fill = tsnr, color=tsnr)) +
   scale_fill_gradient2(high='red',mid='green',low='blue',na.value='#FFFFFF00') +
   facet_grid(z~.) +
   #geom_raster() +
     theme_minimal() +
   labs(fill='Angle of\nmax tSNR') + geom_point()
+
+###
+
+b0 <- oro.nifti::readNIfTI('../Figures/tsnr/sub-1_space-MNI152NLin2009cAsym_fmap.nii.gz')@.Data
+b0[mni_mask==0]<-NA
+#system("3dresample -inset ~/.templateflow/tpl-MNI152NLin2009cAsym/tpl-MNI152NLin2009cAsym_res-01_T1w.nii.gz -master ../Figures/tsnr/sub-1_space-MNI152NLin2009cAsym_fmap.nii.gz -prefix MNI_T1w.nii.gz")
+mni <- oro.nifti::readNIfTI('MNI_T1w.nii.gz')@.Data
+
+
+p_anat <- cowplot::plot_grid(ncol=4,
+ rslice_ys(b0) |>
+  ggplot(aes(x = x, y = z, fill = tsnr)) +
+  scale_fill_gradient(high='red',low='blue', limits=c(-150,150),na.value="#FFFFFF00") +
+  facet_grid(y~.) +
+  geom_raster() +
+  theme_minimal() +
+  guides(fill="none"),
+ rslice_zs(b0) |>
+  ggplot(aes(x = x, y = y, fill = tsnr)) +
+  scale_fill_gradient(high='red',low='blue', limits=c(-200,200),na.value="#FFFFFF00") +
+  facet_grid(z~.) +
+  geom_raster() +
+    theme_minimal() +
+  guides(fill="none"),
+ rslice_ys(mni) |>
+  ggplot(aes(x = x, y = z, fill = tsnr)) +
+  scale_fill_gradient(high='gray62',low='grey8') +
+  facet_grid(y~.) +
+  geom_raster() +
+  theme_minimal() +
+  guides(fill="none"),
+ rslice_zs(mni) |>
+  ggplot(aes(x = x, y = y, fill = tsnr)) +
+  scale_fill_gradient(high='gray62',low='grey8') +
+  facet_grid(z~.) +
+  geom_raster() +
+  theme_minimal() +
+  guides(fill="none")
+ )
+ggsave(p_anat,file='b0_and_anat.png')
