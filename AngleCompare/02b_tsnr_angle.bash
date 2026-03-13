9da62c2f3fc435f4fc455ce50a927d2c9f0f8d47
WillForan <willforan@gmail.com>
2026-03-12 22:03:05 -0400

    02b_tsnr_angle.bash: 3depi

--- AngleCompare/02b_tsnr_angle.bash
+++ AngleCompare/02b_tsnr_angle.bash
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
+
+# 20260312-  for 3depi
+mkdir -p ./maxangle_mni/3depi/
+skip-exist ./maxangle_mni/3depi/sub-1_tsnr-3depiall4d.nii.gz \
+  dryrun 3dTcat -prefix __SKIPFILE \
+     ../Data/tsnr/3depi/sub-1_task-{n40,n33,n13,13,20}_acq-3d_tsnr.nii.gz
+printf "%s\n" -40 -33 -13 13 20 | drytee ./maxangle_mni/3depi/angles.txt
+skip-exist ./maxangle_mni/3depi/sub-1_angleatmax-tsnr.nii.gz  \
+  dryrun ../FmapCorrect/angle_at_max.py  \
+        --nosd \
+        -l ./maxangle_mni/3depi/angles.txt \
+        -i ./maxangle_mni/3depi/sub-1_tsnr-3depiall4d.nii.gz  \
+        -m ../Data/preproc/bids-3depi/fmriprep-25.2.3/sub-1/func/sub-1_task-13_acq-3d_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz \
+        -o __SKIPFILE
+
+# Oct 27 2025
 test -r ./maxangle_mni/sub-1_tsnr-all4d.nii.gz ||
- 3dTcat -prefix ./maxangle_mni/sub-1_tsnr-all4d.nii.gz \
+ dryrun 3dTcat -prefix ./maxangle_mni/sub-1_tsnr-all4d.nii.gz \
         ../Data/tsnr/sub-1_task-restn39_tsnr.nii.gz \
         ../Data/tsnr/sub-1_task-restn33_tsnr.nii.gz \
         ../Data/tsnr/sub-1_task-restn26_tsnr.nii.gz \
@@ -13,7 +29,7 @@ test -r ./maxangle_mni/sub-1_tsnr-all4d.nii.gz ||
         ../Data/tsnr/sub-1_task-rest20_tsnr.nii.gz
 
 test -r ./maxangle_mni/sub-1_angleatmax-tsnr.nii.gz  ||
-  ../FmapCorrect/angle_at_max.py  \
+  dryrun ../FmapCorrect/angle_at_max.py  \
         --nosd \
         -l ../FmapCorrect/angle.txt \
         -i ./maxangle_mni/sub-1_tsnr-all4d.nii.gz  \
@@ -21,15 +37,16 @@ test -r ./maxangle_mni/sub-1_angleatmax-tsnr.nii.gz  ||
         -o ./maxangle_mni/sub-1_angleatmax-tsnr.nii.gz 
 
 test -r maxangle_mni/tsnr-min.nii.gz ||
- 3dTstat \
+ dryrun 3dTstat \
   -prefix maxangle_mni/tsnr-min.nii.gz \
   -min ./maxangle_mni/sub-1_tsnr-all4d.nii.gz 
 test -r maxangle_mni/tsnr-max.nii.gz ||
- 3dTstat \
+ dryrun 3dTstat \
   -prefix maxangle_mni/tsnr-max.nii.gz \
   -max ./maxangle_mni/sub-1_tsnr-all4d.nii.gz 
 
-3dcalc -n maxangle_mni/tsnr-min.nii.gz\
+skip-exist maxangle_mni/tsnr-range.nii.gz \
+dryrun 3dcalc -n maxangle_mni/tsnr-min.nii.gz\
        -x maxangle_mni/tsnr-max.nii.gz\
        -expr 'x-n' \
        -prefix maxangle_mni/tsnr-range.nii.gz
