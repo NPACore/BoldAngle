d7c749097ba62e2cb6a442c1f6ab1395e7c91faa
WillForan <willforan@gmail.com>
2026-04-27 21:26:41 -0400

    3depi_iso/02b_tsnr_angle.bash: iso as 2x2x2

--- 3depi_iso/02b_tsnr_angle.bash
+++ 3depi_iso/02b_tsnr_angle.bash
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
+set -xuo pipefail
 # calc best angle based on tsnr
 # copied from ../AngleCompare/02b_tsnr_angle.bash
 # still depends on ../AngleCompare/02_tsnr.bash to generate tsnr files
 
-mkdir -p ../AngleCompare/maxangle_mni/3depi_iso/
-skip-exist ./maxangle_mni/3depi_iso/sub-1_tsnr-3depi222all4d.nii.gz \
-  dryrun 3dTcat -prefix __SKIPFILE \
-     ../Data/tsnr/3depi_iso/sub-1_task-rest_acq-{n40,n33,n13,p13,p20}_tsnr.nii.gz
+sub=sub-1iso3d
+maxang_dir=../AngleCompare/maxangle_mni/3depi2x2x2 # creating now
+tsnr_dir=../Data/tsnr/3depi2x2x2 # from ../AngleCompare/02_tsnr.bash
+prep_dir=../Data/preproc/bids-3depi2x2x2/fmriprep-25.2.3
+angles=(-40 -33 -13 13 20)
+tsnr_files=($tsnr_dir/${sub}_task-rest_acq-{n40,n33,n13,p13,p20}_tsnr.nii.gz)
 
-printf "%s\n" -40 -33 -13 13 20 | drytee ./maxangle_mni/3depi_iso/angles.txt
-skip-exist ./maxangle_mni/3depi_iso/sub-1_angleatmax-tsnr.nii.gz  \
+dryrun mkdir -p $maxang_dir
+skip-exist $maxang_dir/${sub}_tsnr-3depi222all4d.nii.gz \
+  dryrun 3dTcat -prefix __SKIPFILE ${tsnr_files[@]}
+
+printf "%s\n" ${angles[@]} | drytee $maxang_dir/angles.txt
+skip-exist $maxang_dir/${sub}_angleatmax-tsnr.nii.gz  \
   dryrun ../FmapCorrect/angle_at_max.py  \
         --nosd \
-        -l ./maxangle_mni/3depi_iso/angles.txt \
-        -i ./maxangle_mni/3depi_iso/sub-1_tsnr-3depi222all4d.nii.gz  \
-        -m ../Data/preproc/bids-3depi2x2x2/fmriprep-25.2.3/sub-1/func/sub-1_task-rest_acq-p13_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz \
+        -l $maxang_dir/angles.txt \
+        -i $maxang_dir/${sub}_tsnr-3depi222all4d.nii.gz  \
+        -m $prep_dir/${sub}/func/${sub}_task-rest_acq-p13_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz \
         -o __SKIPFILE
