d4d131407c03f850b7959b87b620c04a30089844
WillForan <willforan@gmail.com>
2026-04-30 11:52:31 -0400

    feat(tsnr): make sd files

--- AngleCompare/02_tsnr.bash
+++ AngleCompare/02_tsnr.bash
@@ -7,9 +7,17 @@ for bold in \
   ! [[ $bold =~ bids-([^/]*) ]] && echo "acq version not like bids-* in $bold" && continue
   acqver=${BASH_REMATCH[1]}
   out=../Data/tsnr/$acqver/$(basename $bold| sed 's/_space.*/_tsnr.nii.gz/')
-  test -d $(dirname $out) || dryrun mkdir -p  "$_"
+  # skip when we have output (needed to comment out for making sd below)
   test -r $out && echo "# have $out" && continue
+
+  test -d $(dirname $out) || dryrun mkdir -p  "$_"
   mask=${bold/desc-preproc_bold/desc-brain_mask}
-  test -r $out || MASK=$mask dryrun tsnr $bold $out
+  test -r $out || \
+	  MASK=$mask dryrun tsnr $bold $out
+
+  # 20260430 - also calculate sd
+  test -r ${out/_tsnr.nii.gz/_sd.nii.gz} || \
+    dryrun 3dTstat -stdev -prefix $_ -mask $mask $bold
+
   dryrun 3dROIstats -nomeanout -nzmean -minmax -nzsigma -median -mask "$mask" "$out"
 done
