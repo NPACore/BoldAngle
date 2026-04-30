0116ddf3dda588fe39d497783a7c519fb26196e2
WillForan <willforan@gmail.com>
2026-04-30 11:51:00 -0400

    plot plane and combine with B0+anat

--- AngleCompare/03a_tsnr_cmp.R
+++ AngleCompare/03a_tsnr_cmp.R
@@ -5,6 +5,7 @@
 
 
 # 20251023WF - init
+
 pacman::p_load(dplyr, tidyr, oro.nifti, ggplot2, stringr)
 #pacman::p_install(ggExtra) # dont shadow any other functions
 tsnr_fname <- Sys.glob('../Data/tsnr/sub-[12]_task-rest*_tsnr.nii.gz')
@@ -16,6 +17,7 @@ tsnr_3ds <- lapply(tsnr_fname, function(f){
     return(nii)
 })
 
+### tsnr
 
 # globals tsnr_3ds and tsrn_fname
 # could just use tsnr_fname and load data, but caching those in tsnr_3ds for other use
@@ -27,6 +29,7 @@ dim_mean_df <- function(fi, xyi)
                nnz=apply(tsnr_3ds[[fi]],xyi,
                                \(d) length(which(d!=0))),
                dim=c('x','y','z')[xyi],
+               source='tsnr',
                i=1:dim(tsnr_3ds[[fi]])[xyi])
 
 # dims = 77 95 82
@@ -37,37 +40,116 @@ pdata <- lapply(1:3,
              bind_rows()|>
              filter(nnz>nnz_lims[di])) |>
     bind_rows()
-#x <- lapply(seq_along(tsnr_fname), dim_mean_df, xyi=1) |> bind_rows()|> filter(nnz>4000)
-#y <- lapply(seq_along(tsnr_fname), dim_mean_df, xyi=2) |> bind_rows() |> filter(nnz>3000)
-#z <- lapply(seq_along(tsnr_fname), dim_mean_df, xyi=3) |> bind_rows() |> filter(nnz>3000)
-#pdata<-rbind(x,y)
 
 angles_for_line <- c(20, -13, -39)
-tsnr_plane <- ggplot(pdata) +
+# where interesting things happen -- lines swap
+Y_IDX <- c(21,45,63)
+Z_IDX <- c(25,60, 65)
+pdata_nox <- pdata |> filter(dim!='x')
+tsnr_plane <- ggplot(pdata_nox) +
     aes(x=i, y=tsnr_mean,
         color=angle,
         shape=subj,
         linetype=subj,
         #size=nnz,
         group=as.factor(paste(subj,angle))) +
-    geom_point(alpha=.2) +
-    geom_smooth(data=pdata|>filter(angle %in% angles_for_line),
+    # annotate interesting planes
+    geom_vline(data=rbind(data.frame(x=Y_IDX, dim='y'),
+                          data.frame(x=Z_IDX, dim='z')),
+               aes(xintercept=x,
+                   group=NULL,linetype=NULL,
+                   shape=NULL,color=NULL),
+               color='red')+
+    #geom_point(alpha=.2) +
+    geom_smooth(data=pdata_nox|>filter(angle %in% angles_for_line),
                 method = 'gam',
                 se=FALSE) +
-    scale_color_gradient2(high='red',mid='green',low='blue') +
+    scale_color_gradient2(high='red',mid='green',low='blue', breaks=round(unique(pdata_nox$angle))) +
     facet_grid(dim~.) +
     theme_minimal() +
     labs(title='Average tSNR by cross section',
          x='voxel index',
          y='Median tSRN  (brain masked)')
-ggsave(tsnr_plane, 'tsnr_by_plane.png')
+
+ggsave(tsnr_plane, file="images/tsnr_per_plane.png")
+
+
+## magnitude of disortoin corrected quick 10 angle epi sequence
+# TODO: use mni version?
+sdc_a10_files <- Sys.glob('../FmapCorrect/wf/human-largefov/sub-[12]/epi_undistorted_masked.nii.gz')
+sdc_a10 <- lapply(sdc_a10_files, \(f) oro.nifti::readNIfTI(f)@.Data)
+a10_angle <- read.table('../FmapCorrect/angle.txt')$V1
+angle_is <- c(1,5,10) # a10_angle[angle_is]  -39.90 -13.28  20.00
+a10_slice_mean_df <- function(si, ai, xyi) {
+    m3d <- sdc_a10[[si]][,,,ai]
+    data.frame(angle=a10_angle[ai],
+               subj=str_extract(sdc_a10_files[si],'sub-[^/_-]+'),
+               tsnr_mean=apply(m3d, xyi, \(d) median(d[d!=0])),
+               nnz=apply(m3d,xyi, \(d) length(which(d!=0))),
+               source='shortepi',
+               dim=c('x','y','z')[xyi],
+               i=1:dim(m3d)[xyi])
+}
+a10_pdata <- lapply(1:3,
+  \(xyi) lapply(1:2,
+    \(si) lapply(angle_is,
+       \(ai) a10_slice_mean_df(si,ai, xyi) |>
+             bind_rows())|>
+      bind_rows())) |>
+    bind_rows()
+## full rest
+a10_rest_f <- Sys.glob('../Data/preproc/fmriprep-25.2.3/sub-*/func/sub-*_task-rest*_space-MNI152NLin2009cAsym_desc-mean_bold.nii.gz')
+a10_rest_mean_df <- function(f, xyi) {
+    m3d <- oro.nifti::readNIfTI(f)@.Data
+    data.frame(angle=angle_from_taskname(f),
+               subj=str_extract(f,'sub-[^/_-]+'),
+               tsnr_mean=apply(m3d, xyi, \(d) median(d[d!=0])),
+               nnz=apply(m3d,xyi, \(d) length(which(d!=0))),
+               source='rest',
+               dim=c('x','y','z')[xyi],
+               i=1:dim(m3d)[xyi])
+}
+a10_rest_pdata <- lapply(1:3,
+       \(di) lapply(a10_rest_f |> grepv(pat="task-rest(n39|n13|20)"),
+                    a10_rest_mean_df, xyi=di) |>
+             bind_rows()|>
+             filter(nnz>nnz_lims[di])) |>
+    bind_rows()
+
+
+a10_plane <- rbind(a10_pdata,
+                   a10_rest_pdata,
+                   pdata |> filter(angle %in% angles_for_line)) |>
+    ggplot() +
+    aes(x=i, y=tsnr_mean,
+        color=angle,
+        shape=subj,
+        linetype=subj,
+        #size=nnz,
+        group=as.factor(paste(subj,angle))) +
+    # annotate interesting planes
+    #geom_point(alpha=.2) +
+    geom_smooth(method = 'gam',
+                se=FALSE) +
+    scale_color_gradient2(high='red',mid='green',low='blue',
+                          limits=c(-40,20),
+                          breaks=unique(a10_rest_pdata$angle)) +
+    facet_wrap(source~dim, scales="free") +
+    theme_minimal() +
+    labs(title='EPI magnituded by cross section',
+         x='voxel index',
+         y='Median magnitude  (brain masked)')
+
+ggsave(file="images/a10_plane.png", a10_plane)
+
+
 
 subj1_idx <- which(grepl('sub-1', tsnr_fname))
 alltsnr_sub1 <- as.matrix(unlist(tsnr_3ds[subj1_idx]))
 dim(alltsnr_sub1) <- c(dim(tsnr_3ds[[1]]),length(subj1_idx))
 
 # make sure we put the matrix back together correctly
-stopifnot(all(alltsnr[,,,subj1_idx[2]] == tsnr_3ds[[subj1_idx[2]]]))
+stopifnot(all(alltsnr_sub1[,,,subj1_idx[2]] == tsnr_3ds[[subj1_idx[2]]]))
 
 anglemax3d <- apply(alltsnr_sub1,1:3,which.max)
 anglemax3d[alltsnr_sub1[,,,1]==0] <- NA
@@ -80,7 +162,7 @@ dim(anglemax3d) <- dim(alltsnr_sub1)[1:3]
 
 ## volumetric plots
 rslice_ys <- function(data)
-    lapply(c(20,45,65),
+    lapply(Y_IDX,
            \(y) reshape2::melt(data[,y,],
                                varnames=c("x","z"),
                                value.name='tsnr') |>
@@ -88,37 +170,28 @@ rslice_ys <- function(data)
         bind_rows()
 
 rslice_zs <- function(data)
-    lapply(c(25,55, 65),
+    lapply(Z_IDX,
            \(z) reshape2::melt(data[,,z],
                                varnames=c("x","y"),
                                value.name='tsnr') |>
                 mutate(z=z)) |>
         bind_rows()
 
-cowplot::plot_grid(ncol=3,
- tsnr_plane,
- rslice_ys(anglemax3d) |>
+
+tsnr_angle_y <- rslice_ys(anglemax3d) |>
   ggplot(aes(x = x, y = z, fill = tsnr)) +
   scale_fill_gradient2(high='red',mid='green',low='blue',na.value='#FFFFFF00') +
   facet_grid(y~.) +
   geom_raster() +
   theme_minimal() +
-  guides(fill="none"),
- rslice_zs(anglemax3d) |>
+  guides(fill="none")
+tsnr_angle_z <- rslice_zs(anglemax3d) |>
   ggplot(aes(x = x, y = y, fill = tsnr)) +
   scale_fill_gradient2(high='red',mid='green',low='blue',na.value='#FFFFFF00') +
   facet_grid(z~.) +
   geom_raster() +
     theme_minimal() +
-  labs(fill='Angle of\nmax tSNR') )
-
-p<- rslice_zs(anglemax3d) |>
-  ggplot(aes(x = x, y = y, fill = tsnr, color=tsnr)) +
-  scale_fill_gradient2(high='red',mid='green',low='blue',na.value='#FFFFFF00') +
-  facet_grid(z~.) +
-  #geom_raster() +
-    theme_minimal() +
-  labs(fill='Angle of\nmax tSNR') + geom_point()
+  labs(fill='Angle of\nmax tSNR')
 
 ###
 
@@ -130,38 +203,71 @@ b0[mni_mask==0]<-NA
 mni <- oro.nifti::readNIfTI('MNI_T1w.nii.gz')@.Data
 
 
+#cowplot::plot_grid(ncol=3,)
+oob <- abs(b0)>200 & !is.na(b0)
+b0_pdata <- b0; b0_pdata[oob] <- sign(b0[oob])*200
 
-p_anat <- cowplot::plot_grid(ncol=4,
- rslice_ys(b0) |>
+p_b0_y <- rslice_ys(b0_pdata) |>
   ggplot(aes(x = x, y = z, fill = tsnr)) +
-  scale_fill_gradient(high='red',low='blue', limits=c(-150,150),na.value="#FFFFFF00") +
+  scale_fill_gradient(high='red',low='blue', limits=c(-200,200),na.value="#FFFFFF00") +
   facet_grid(y~.) +
   geom_raster() +
   theme_minimal() +
-  guides(fill="none"),
- rslice_zs(b0) |>
+  guides(fill="none")
+
+p_b0_z <- rslice_zs(b0_pdata) |>
   ggplot(aes(x = x, y = y, fill = tsnr)) +
-  scale_fill_gradient(high='red',low='blue', limits=c(-200,200),na.value="#FFFFFF00") +
+  scale_fill_gradient(high='red',low='blue', limits=c(-200,200),na.value="#FFFFFF00",breaks=c(-200,0,200)) +
   facet_grid(z~.) +
   geom_raster() +
-    theme_minimal() +
-  guides(fill="none"),
- rslice_ys(mni) |>
+  theme_minimal() # + guides(fill="none")
+
+p_mni_y <- rslice_ys(mni) |>
   ggplot(aes(x = x, y = z, fill = tsnr)) +
   scale_fill_gradient(high='gray62',low='grey8') +
   facet_grid(y~.) +
   geom_raster() +
   theme_minimal() +
-  guides(fill="none"),
- rslice_zs(mni) |>
+  guides(fill="none")
+p_mni_z <- rslice_zs(mni) |>
   ggplot(aes(x = x, y = y, fill = tsnr)) +
   scale_fill_gradient(high='gray62',low='grey8') +
   facet_grid(z~.) +
   geom_raster() +
   theme_minimal() +
   guides(fill="none")
- )
-ggsave(p_anat,file='b0_and_anat.png')
+
+no_facet <- theme(strip.background = element_blank(),
+                 strip.text.y = element_blank())
+#no_y <- scale_y_discrete(labels = NULL, breaks = NULL)
+p_anat <- cowplot::plot_grid(ncol=3,
+  tsnr_angle_y + theme_void() + no_facet,
+  p_b0_y + theme_void() + no_facet,
+  p_mni_y + theme_void(),
+  tsnr_angle_z + theme_void() + no_facet +
+                 theme(legend.position = "none"),
+  p_b0_z + theme_void() + no_facet +
+           theme(legend.position = c(.4,.3),
+                 legend.direction = "horizontal") + labs(fill='B0'),
+  p_mni_z + theme_void())
+ggsave(p_anat,file='images/anat_tsnr_b0_mni.png')
+
+mktitle <- function(title)  cowplot::ggdraw() +
+  cowplot::draw_label(title, fontface = 'bold', x = 0, hjust = 0)
+  #+theme(plot.margin = margin(0, 0, 0, 7))
+
+p_fig3 <- cowplot::plot_grid(ncol=2,
+                             rel_heights = c(0.1, 1),
+                             mktitle("  A. In-plane tSNR"),
+                             mktitle("B. Angle Map at Max. ΔB0. Anatomy "),
+                             tsnr_plane+labs(title=NULL),
+                             p_anat)
+
+ggsave(p_fig3,
+       file='images/fig3_tsnr_b0_mni.png',
+       width=8.39,height=7.56)
+
+
 
 ### cor between tsnr angle at max and scan time calculation (epi mag) 
 atscanmax <- oro.nifti::readNIfTI('./maxangle_mni/sub-1_a10_space-MNI_maxangle.nii.gz')@.Data
@@ -195,6 +301,16 @@ cbind(
     rle(sort(round(anglemax3d[tsnr_range>20]))) |> with(data.frame(tsnr=values,ntsnrvox=lengths))) |>
     knitr::kable()
 
+p_best_angle_dist <- rbind(
+  data.frame(from="a10epi",angle=atscanmax[TRUE]),
+  data.frame(from="tsnr",  angle=anglemax3d[TRUE])) |>
+    ggplot() + aes(x=angle, fill=from) +
+    geom_histogram(position='dodge') +
+   theme_minimal() +
+   labs(title='Distribution of best angle assignments')
+ggsave(p_best_angle_dist,file="images/max_angle_distribution.png",
+       width=5.99, height=3.32)
+
 
 angle_max <- data.frame(
     tsnr=as.vector(anglemax3d), 
@@ -204,4 +320,4 @@ p_angle <- ggplot(angle_max) +
     aes(x=b0, y=epi, color=b0) +
     geom_jitter() +
     theme_minimal()
-ggsave(p_angle,file="max_angle_corr.png")
+#ggsave(p_angle,file="max_angle_corr.png")
