f087f73335f2d748d2f906e1ba2e0b855a12a85d
WillForan <willforan@gmail.com>
2026-05-11 06:00:07 -0400

    fix: seq_params now includes matrix size

--- Figures/parameters.R
+++ Figures/parameters.R
@@ -1,4 +1,5 @@
 #!/usr/bin/env Rscript
+pacman::p_load(dplyr,gridExtra)
 # expect to run from repo root (pwd one above Figures/)
 seq_params <- read.csv('seq_params.csv',sep=",")
 #pacman::p_install('mmtable2')
@@ -12,7 +13,6 @@ seq_params <- read.csv('seq_params.csv',sep=",")
 # kable_styling(position="center", font_size=25, full_width=T) |>
 # as_image(file='Figures/parameters.png', width=7, height=5, density=300)
 
-library(gridExtra)
 seqtheme <- ttheme_default(core=list(fg_params=list(hjust=0,x=0)),
                       rowhead=list(fg_params=list(hjust=1, x=0.95, fontface="bold")))
 grid::grid.newpage()
@@ -20,6 +20,6 @@ seq_params_fmt <- seq_params|>
     select(-seq) |>
    mutate(across(everything(),\(x) ifelse(is.na(x),"",x)|>gsub(';','\n',x=_)))
 rownames(seq_params_fmt) <- seq_params$seq
-png("Figures/parameters.png", width=5.5, height=3, units="in", res=300)
+png("Figures/parameters.png", width=7, height=3, units="in", res=300)
 tableGrob(seq_params_fmt, theme=seqtheme) |> grid::grid.draw()
 dev.off()
